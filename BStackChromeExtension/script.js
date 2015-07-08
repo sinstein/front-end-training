ea7fb77a1351ba49b4603ba39017dd837a0d8b83
Saurav Kothari <sauravkothari2@gmail.com>
2015-07-08 11:30:43 +0530

    Url check | Code comments

--- BStackChromeExtension/script.js
+++ BStackChromeExtension/script.js
@@ -1,7 +1,12 @@
-$(".errors").hide();
+$(".option_errors").hide();
+$(".url_errors").hide();
 
 $(document).ready(function(){
   $("#urlForm").hide();
+  /*
+  Fetch data from BrowserStack RESP API (http://www.browserstack.com/list-of-browsers-and-platforms.json?product=live)
+  Concerned with only Desktop Browsers
+  */
   $.getJSON( "http://www.browserstack.com/list-of-browsers-and-platforms.json?product=live" ,
   function( data ) {
     var desktopBrowsers = data["desktop"];
@@ -10,6 +15,9 @@ $(document).ready(function(){
     var browserNames = $('<select required id="bNames" class="dropDownList"/>');
     var browserVersions = $('<select required id="bVersion" class="dropDownList"/>');
 
+    /*
+    Preparing list of available Desktop OS
+    */
     $('<option />').appendTo(os);
     $.each( desktopBrowsers, function( key, val ) {
       $('<option />', {value: val["os_version"], text: val["os_display_name"], class: val["os"] }).appendTo(os);
@@ -22,11 +30,14 @@ $(document).ready(function(){
       var osFamily = $('#os :selected').attr("class");
       var browserList = [];
 
-      browserNames.empty();
       browserVersions.empty();
+      /*
+      Preparing list of compatible browsers
+      */
+      browserNames.empty();
       $.each(desktopBrowsers, function(key,val) {
         if(value == val["os_display_name"]) {
-          browserList = createSortableList(this["browsers"]).sort();
+          browserList = createSortableList(this["browsers"]);
           var length = browserList.length
           for (var i = 0; i < length; i++) {
             $('<option />', {value: browserList[i]["name"], text: browserList[i]["name"]}).appendTo(browserNames);
@@ -36,11 +47,15 @@ $(document).ready(function(){
           var browserName = $('#bNames :selected').text();
           var length = browserList.length;
 
+          /*
+          Preparing list of browser versions
+          If the OS is changed midway, browser versions must be updated
+          */
           browserVersions.empty();
           for (var i = 0; i < length; i++) {
             if(browserList[i]["name"] == browserName) {
               var length2 = browserList[i]["versions"].length;
-              browserList[i]["versions"].sort();
+              browserList[i]["versions"].sort().reverse();
               //console.log(browserList[i]["versions"]);
               for(var j = 0; j < length2; j++) {
                 $('<option />', { value: browserList[i]["versions"][j], text: browserList[i]["versions"][j] }).appendTo(browserVersions);
@@ -50,6 +65,9 @@ $(document).ready(function(){
           }
           browserVersions.insertAfter("#bNames");
 
+          /*
+          Changing browser selection must trigger update of versions
+          */
           $("#bNames").change(function(){
             var browserName = $('#bNames :selected').text();
             var length = browserList.length;
@@ -58,7 +76,7 @@ $(document).ready(function(){
             for (var i = 0; i < length; i++) {
               if(browserList[i]["name"] == browserName) {
                 var length2 = browserList[i]["versions"].length;
-                browserList[i]["versions"].sort();
+                browserList[i]["versions"].sort().reverse();
                 //console.log(browserList[i]["versions"]);
                 for(var j = 0; j < length2; j++) {
                   $('<option />', { value: browserList[i]["versions"][j], text: browserList[i]["versions"][j] }).appendTo(browserVersions);
@@ -71,12 +89,18 @@ $(document).ready(function(){
 
           $("#urlForm").show();
 
+          /*
+          Get the url of current tab
+          */
           chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
              var activeTabUrl = arrayOfTabs[0].url;
              console.log(activeTabUrl);
              $('input[name=urlToTest]').val(activeTabUrl);
           });
 
+          /*
+          Handling user submit/click
+          */
           $('input#submit').click(function(event){
             var newTask = $('input[name=urlToTest]').val();
             $('input[name=urlToTest]').val("");
@@ -95,24 +119,35 @@ $(document).ready(function(){
 });
 
 
+/*
+Runs BrowserStack LIVE session if a valid request is made
+Checks for url validity if all options are selected
+*/
 var validRequestCheck = function(os, os_version, browser, browser_version, url) {
+
     if(os && os_version && browser && browser_version && url) {
-      var test_address = "https://www.browserstack.com/start#"
-                          + "&os=" + os
-                          + "&os_version=" + os_version
-                          + "&browser=" + browser
-                          + "&browser_version=" + browser_version
-                          + "&scale_to_fit=true"
-                          + "&start=true"
-                          + "&url=" + url;
-      window.open(test_address);
+      if(isUrlValid(url)) {
+        var test_address = "https://www.browserstack.com/start#"
+                            + "&os=" + os
+                            + "&os_version=" + os_version
+                            + "&browser=" + browser
+                            + "&browser_version=" + browser_version
+                            + "&scale_to_fit=true"
+                            + "&start=true"
+                            + "&url=" + url;
+        window.open(test_address);
+      }
     }
     else {
-      $(".errors").show();
-      setTimeout(function() { $(".errors").hide(); }, 3000);
+      $(".option_errors").show();
+      setTimeout(function() { $(".option_errors").hide(); }, 3000);
     }
 }
 
+/*
+Creates list of browsers for chosen OS
+Sorted to display same order everytime
+*/
 var createSortableList = function(compatibleBrowsers) {
   var browserList = [];
   $.each(compatibleBrowsers, function(key, val) {
@@ -130,9 +165,18 @@ var createSortableList = function(compatibleBrowsers) {
       browserList.push(temp);
     }
   });
-  return browserList
+  return browserList.sort().reverse()
 }
 
+var isUrlValid = function(url) {
+    if(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(url)) {
+      return true
+    }
+    else {
+      $(".url_errors").show();
+      setTimeout(function() { $(".url_errors").hide(); }, 3000);
+    }
+}
 
 var contains = function(array_name, value) {
   var length = array_name.length;
